1a57fec974acf98478db54afda5fc93078dc9686
LightmanL <seasea1@live.hk>
2016-01-16 19:04:07 +0000

    Finish log in page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #RTeturn the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "Fill in the blanks w/ automated grading"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Fill in the blanks w/ automated grading"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Fill in the blanks w/ automated grading"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "Fill in the blanks w/ automated grading"
+  end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Fill in the blanks w/ automated grading"
+  end
 end
