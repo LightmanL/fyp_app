1cd02286ec07be75a8ae9e86783a738fb0a36258
LightmanL <seasea1@live.hk>
2016-01-20 17:55:08 +0000

    Finish log in/log out

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,22 @@ class StaticPagesController < ApplicationController
   end
   
   def contact
-  end  
+  end
   
+  def create
+    user = User.find_by(email: params[:static_pages][:email].downcase)
+    if user && user.authenticate(params[:static_pages][:password])
+      log_in user
+      params[:static_pages][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'home'
+    end
+  end
+  
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end  
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,49 @@
 module StaticPagesHelper
+  
+  # Logs in the given user.Actually this should be done in SessionHelper,
+  # but in this situation the home page actually is the login page, too.
+  # That's why the login helper put in here.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # Remembers a user on a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # Return the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
+  # Return true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+  
+    # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
 end
