3b44aa8a05af67d99c9141c3a0fa2603350adf0f
LightmanL <seasea1@live.hk>
2016-01-20 18:17:42 +0000

    Improves test of the  me$ checkbox

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,11 +12,11 @@ class StaticPagesController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:static_pages][:email].downcase)
-    if user && user.authenticate(params[:static_pages][:password])
-      log_in user
-      params[:static_pages][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+    @user = User.find_by(email: params[:static_pages][:email].downcase)
+    if @user && @user.authenticate(params[:static_pages][:password])
+      log_in @user
+      params[:static_pages][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'home'
